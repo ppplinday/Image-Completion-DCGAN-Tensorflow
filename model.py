866292cec7d4fae7648de5a8883392a1801c0cc8
ppplinday <peilinzhou666@gmail.com>
2018-01-23 19:18:37 -0800

    v2.0.4

--- model.py
+++ model.py
@@ -406,8 +406,7 @@ class DCGAN:
 					np.savetxt(f, z[img:img+1])
 
 			if i % 50 == 0:
-				print(i, np.mean(loss[0:batch_size_z]))
-				print(np.array(g).shape)
+				
 				print("Iter: [{:4d}/{:4d}] time: {:4.4f}, mean_loss: {:.8f}, mean_g: {:.8f}".format(
 					i, config.nIter, time.time() - start_time, np.mean(loss[0:batch_size_z]), np.mean(g[0:batch_size_z])))
 				imgName = os.path.join(config.completion_dir, 'hats_imgs/{:04d}.png'.format(i))
