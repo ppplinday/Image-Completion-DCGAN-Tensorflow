02278c7f4afbe03427339e58e74ddc44ec3d5077
ppplinday <peilinzhou666@gmail.com>
2018-01-23 17:18:46 -0800

    completion1

---
+++ train_completion.py
@@ -0,0 +1,11 @@
+import os
+import scipy.misc
+import numpy as np
+import config
+from model import DCGAN
+import tensorflow as tf
+
+with tf.Session() as sess:
+	dcgan = DCGAN(sess)
+
+	dcgan.completed()
